fb4c8950c30fc9bf5b6467f01a4ce108d6cb619a
hysios <hysios@gmail.com>
2010-12-22 19:01:29 +0800

    incompleted setup devise ,don't download

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rush::Application.routes.draw do
+  devise_for :models
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -12,7 +14,9 @@ Rush::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+	resources :admins
+	
+	resource :home
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,11 +52,11 @@ Rush::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
