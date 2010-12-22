77f9d125af557740e9a1ddb5eb95556c11687633
hysios <hysios@gmail.com>
2010-12-22 12:53:23 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rush::Application.config.secret_token = '9a17689803d9919dc576fd8db7e5ecd95f48a107fcb4a305dfdf962a2226f2c82d5e6fda11d2ece50e23ed3a6665b952420a00bffddaf0493eb5705dff51e22d'
